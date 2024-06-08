b8b7f8765396cff306d66a2b86209e24f0f250bc
punyjin <waphagisd@hotmail.com>
2024-06-08 20:49:29 +0700

    Add Source Code
    Add a Some Example Code For Update and Extract zipfile from URL

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("iannnnn-DOG", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Launcher Batch";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.IndianRed;
+            this.progressBar1.Location = new System.Drawing.Point(-1, 434);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(493, 16);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(455, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(424, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(155, 376);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(170, 62);
+            this.btn_start.TabIndex = 4;
+            this.btn_start.Text = "Open Launcher";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private DateTime lastUpdateTime; //ตัวแปร เวลา
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            // เพิ่ม MouseDown event handler ให้กับ Form
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_start.Enabled = false;
+            btn_start.Hide();
+            string url = "http://25.64.193.152/update/update.zip"; //ที่อยู่ของไฟล์
+            string localPath = "update.zip"; //ชื่อไฟล์
+            try
+            {
+                DownloadFile(url, localPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error 001: {ex.Message}");
+            }
+        }
+        private void DownloadFile(string url, string localPath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri(url), localPath);
+            }
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // อัปเดต UI ทุก ๆ 1500 มิลลิวินาที ( 1.5 วินาที )
+            if ((DateTime.Now - lastUpdateTime).TotalMilliseconds >= 1500)
+            {
+                progressBar1.Value = e.ProgressPercentage;
+                label1.Text = $"Downloaded {e.BytesReceived / 1024.0 / 1024.0:F2} MB of {e.TotalBytesToReceive / 1024.0 / 1024.0:F2} MB ({e.ProgressPercentage}%)";
+                lastUpdateTime = DateTime.Now;
+            }
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show($"Error 002: {e.Error.Message}");
+                return;
+            }
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Error 003: Download cancelled");
+                return;
+            }
+
+            string zipPath = "update.zip"; //ชื่อไฟล์
+            string extractPath = AppDomain.CurrentDomain.BaseDirectory; //ที่อยู่ของโปรแกรม ควรจะอยู่ที่เดียวกันกับ Launcher
+
+            // ตรวจสอบว่ามีไฟล์ .zip อยู่แล้วหรือไม่
+            if (File.Exists(zipPath))
+            {
+                try
+                {
+                    ZipFile.ExtractToDirectory(zipPath, extractPath);
+                    File.Delete(zipPath);
+                    btn_start.Enabled = true;
+                    btn_start.Show();
+                    progressBar1.Hide();
+                    label1.Hide();
+                    //MessageBox.Show("Download and extraction completed!");
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show($"Error 004: {ex.Message}");
+                }
+                catch (InvalidDataException ex)
+                {
+                    MessageBox.Show($"Error 005: {ex.Message}");
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show($"Error 006: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error 007: {ex.Message}");
+                }
+            }
+        }
+
+
+        // Import ฟังก์ชัน ReleaseCapture และ SendMessage จาก user32.dll
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        // ค่าคงที่สำหรับการส่งข้อความ
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, (IntPtr)HT_CAPTION, IntPtr.Zero);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Error 003: Download cancelled");
+            Application.Exit();//ปุ่มปิดโปรแกรม
+            /*
+             * จริงๆสามารถเติมแต่งส่วนนี้ได้ เผื่อจะถาม User ว่าต้องการปิดโปรแกรมตอนนี้เลยหรือไม่ ?
+             * */
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized; //ปุ่นย่อโปรแกรม
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            Process.Start("Launcher.exe"); //เปิด Process ที่ตั้งไว้
+            Application.Exit();
+        }
+    }
+}
