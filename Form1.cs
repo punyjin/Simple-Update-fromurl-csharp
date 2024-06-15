9dedb0a9a51359e50f54fd8779a634283aa5ba93
xorbit <91112547+punyjin@users.noreply.github.com>
2024-06-16 02:01:23 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using LC_Update;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,14 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         private DateTime lastUpdateTime; //ตัวแปร เวลา
+        public string extractPath = LC_Update.Properties.Settings.Default.Path_Location; // เพิ่มตัวแปรสำหรับเก็บเส้นทางที่เลือก
         public Form1()
         {
             InitializeComponent();
@@ -30,86 +33,181 @@ namespace WindowsFormsApp1
         {
             btn_start.Enabled = false;
             btn_start.Hide();
-            string url = "http://127.0.0.1/update/update.zip"; //ที่อยู่ของไฟล์
-            string localPath = "update.zip"; //ชื่อไฟล์
+            Check_Install_Path(); // ตรวจสอบตำแหน่งไฟล์
+            string versionUrl = "http://25.64.193.152/update/version.xml"; //ที่อยู่ของไฟล์เวอร์ชัน
+            string localVersionPath = "version.xml"; //ชื่อไฟล์เวอร์ชัน
+
             try
             {
-                DownloadFile(url, localPath);
+                DownloadFile(versionUrl, localVersionPath, CheckVersion);
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error 001: {ex.Message}");
             }
         }
-        private void DownloadFile(string url, string localPath)
+
+        private void Check_Install_Path()
+        {
+            if (!LC_Update.Properties.Settings.Default.Install_First_Time)
+            {
+                using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+                {
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        extractPath = folderDialog.SelectedPath;
+                        LC_Update.Properties.Settings.Default.Path_Location = extractPath;
+                        LC_Update.Properties.Settings.Default.Install_First_Time = true;
+                        LC_Update.Properties.Settings.Default.Save();
+                    }
+                    else
+                    {
+                        MessageBox.Show("กรุณาเลือกที่อยู่ติดตั้งก่อน", "LC_Update");
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                extractPath = LC_Update.Properties.Settings.Default.Path_Location;
+            }
+        }
+
+        private void DownloadFile(string url, string localPath, Action callback)
         {
             using (WebClient client = new WebClient())
             {
-                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
-                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileCompleted += (sender, e) =>
+                {
+                    if (e.Error != null)
+                    {
+                        MessageBox.Show($"Error 002: {e.Error.Message}");
+                        return;
+                    }
+                    if (e.Cancelled)
+                    {
+                        MessageBox.Show("Error 003: ยกเลิกการดาวน์โหลด !");
+                        return;
+                    }
+                    callback?.Invoke();
+                };
                 client.DownloadFileAsync(new Uri(url), localPath);
             }
         }
 
-        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        private void CheckVersion()
         {
-            // อัปเดต UI ทุก ๆ 1500 มิลลิวินาที ( 1.5 วินาที )
-            if ((DateTime.Now - lastUpdateTime).TotalMilliseconds >= 1500)
+            string localVersionPath = "version.xml"; //ชื่อไฟล์เวอร์ชัน
+            if (!File.Exists(localVersionPath))
             {
-                progressBar1.Value = e.ProgressPercentage;
-                label1.Text = $"Downloaded {e.BytesReceived / 1024.0 / 1024.0:F2} MB of {e.TotalBytesToReceive / 1024.0 / 1024.0:F2} MB ({e.ProgressPercentage}%)";
-                lastUpdateTime = DateTime.Now;
+                MessageBox.Show("Error 004: ไม่พบไฟล์เวอร์ชัน");
+                return;
+            }
+
+            string installedVersion = LC_Update.Properties.Settings.Default.InstalledVersion;
+            string availableVersion = GetVersionFromXml(localVersionPath);
+
+            if (string.Compare(installedVersion, availableVersion) < 0)
+            {
+                string url = "http://25.64.193.152/update/update.zip"; //ที่อยู่ของไฟล์
+                string localPath = "update.zip"; //ชื่อไฟล์
+                try
+                {
+                    DownloadFile(url, localPath, () => OnDownloadCompleted(availableVersion));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error 001: {ex.Message}");
+                }
+            }
+            else
+            {
+                btn_start.Enabled = true;
+                btn_start.Show();
+                progressBar1.Hide();
+                label1.Hide();
+                MessageBox.Show("ไม่มีการอัปเดตใหม่ในเวลานี้ โปรดรอแพทช์ใหม่ <3","xorbit256");
+                Application.Exit();
             }
         }
 
-        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        private string GetVersionFromXml(string filePath)
         {
-            if (e.Error != null)
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+            XmlNode node = xmlDoc.SelectSingleNode("//version");
+            return node?.InnerText ?? string.Empty;
+        }
+
+        private void OnDownloadCompleted(string newVersion)
+        {
+            string zipPath = "update.zip"; //ชื่อไฟล์ที่อยู่ในระบบฝั่ง Server
+            if (string.IsNullOrWhiteSpace(extractPath))
             {
-                MessageBox.Show($"Error 002: {e.Error.Message}");
-                return;
-            }
-            if (e.Cancelled)
-            {
-                MessageBox.Show("Error 003: Download cancelled");
+                MessageBox.Show("Error 007: Path cannot be the empty string or all whitespace.");
                 return;
             }
 
-            string zipPath = "update.zip"; //ชื่อไฟล์
-            string extractPath = AppDomain.CurrentDomain.BaseDirectory; //ที่อยู่ของโปรแกรม ควรจะอยู่ที่เดียวกันกับ Launcher
-
-            // ตรวจสอบว่ามีไฟล์ .zip อยู่แล้วหรือไม่
+            string extractPathToFolder = extractPath;
             if (File.Exists(zipPath))
             {
                 try
                 {
-                    ZipFile.ExtractToDirectory(zipPath, extractPath);
+                    // ตรวจสอบเวอร์ชันก่อนการติดตั้ง
+                    if (Directory.Exists(extractPathToFolder))
+                    {
+                        string installedVersion = LC_Update.Properties.Settings.Default.InstalledVersion;
+                        if (string.Compare(installedVersion, newVersion) >= 0)
+                        {
+                            MessageBox.Show("The installed version is up-to-date.");
+                            return;
+                        }
+                    }
+
+                    // ลบไฟล์ในที่อยู่ปลายทางหากมีอยู่แล้ว
+                    if (Directory.Exists(extractPathToFolder))
+                    {
+                        Directory.Delete(extractPathToFolder, true);
+                    }
+
+                    ZipFile.ExtractToDirectory(zipPath, extractPathToFolder);
                     File.Delete(zipPath);
+
+                    // อัปเดตเวอร์ชันที่ติดตั้งในระบบ
+                    LC_Update.Properties.Settings.Default.InstalledVersion = newVersion;
+                    LC_Update.Properties.Settings.Default.Save();
+
                     btn_start.Enabled = true;
                     btn_start.Show();
                     progressBar1.Hide();
                     label1.Hide();
-                    //MessageBox.Show("Download and extraction completed!");
+                    MessageBox.Show("Download and extraction completed!");
                 }
                 catch (FileNotFoundException ex)
                 {
                     MessageBox.Show($"Error 004: {ex.Message}");
+                    Application.Exit();
                 }
                 catch (InvalidDataException ex)
                 {
                     MessageBox.Show($"Error 005: {ex.Message}");
+                    Application.Exit();
                 }
                 catch (UnauthorizedAccessException ex)
                 {
                     MessageBox.Show($"Error 006: {ex.Message}");
+                    Application.Exit();
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error 007: {ex.Message}");
+                    LC_Update.Properties.Settings.Default.Path_Location = "";
+                    LC_Update.Properties.Settings.Default.Install_First_Time = false;
+                    LC_Update.Properties.Settings.Default.Save();
+                    Application.Exit();
                 }
             }
         }
-
 
         // Import ฟังก์ชัน ReleaseCapture และ SendMessage จาก user32.dll
         [DllImport("user32.dll")]
@@ -132,22 +230,40 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Error 003: Download cancelled");
-            Application.Exit();//ปุ่มปิดโปรแกรม
-            /*
-             * จริงๆสามารถเติมแต่งส่วนนี้ได้ เผื่อจะถาม User ว่าต้องการปิดโปรแกรมตอนนี้เลยหรือไม่ ?
-             * */
+            DialogResult dialogResult = MessageBox.Show("คุณกำลังจะออกจากโปรแกรม การดำเนินการนี้จะยกเลิกการดาวน์โหลด \r\nต้องการดำเนินการต่อหรือไม่ ?", "xorbit256", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //พื้นที่สำหรับเรียกใช้ ฟังชั่น (เมื่อกด No จะดำเนินการ)
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Minimized; //ปุ่นย่อโปรแกรม
+            this.WindowState = FormWindowState.Minimized; //ปุ่มย่อโปรแกรม
         }
 
         private void btn_start_Click(object sender, EventArgs e)
         {
-            Process.Start("Launcher.exe"); //เปิด Process ที่ตั้งไว้
+            //Process.Start("Launcher.exe"); //เปิด Process ที่ตั้งไว้
             Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    extractPath = folderDialog.SelectedPath;
+                    LC_Update.Properties.Settings.Default.Path_Location = extractPath;
+                    LC_Update.Properties.Settings.Default.Save();
+                }
+            }
         }
     }
 }
